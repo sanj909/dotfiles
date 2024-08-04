c6f8a44200248c2a2f9dab9fe3d3199c5a635cc4
Sanjit Neelam <sanjit@matx.com>
2024-08-03 20:21:41 -0700

    Refactor git aliases

--- config/aliases.sh
+++ config/aliases.sh
@@ -62,37 +62,29 @@ alias 9='cd -9'
 # git
 #-------------------------------------------------------------
 
-alias g="git"
-alias gcl="git clone"
 alias ga="git add"
 alias gaa="git add ."
-alias gau="git add -u"
-alias gc="git commit -m"
+alias gk="git commit -m"
 alias gp="git push"
-alias gpf="git push -f"
 
-alias gg='git gui'
+alias gl="git log --oneline --decorate-refs-exclude=refs/tags -n10"
+alias glf="git log --decorate-refs-exclude=refs/tags"
 alias glog='git log --oneline --all --graph --decorate'
 
 alias gf="git fetch"
-alias gl="git pull"
 
-alias grb="git rebase"
-alias grbm="git rebase master"
-alias grbc="git rebase --continue"
-alias grbs="git rebase --skip"
-alias grba="git rebase --abort"
+alias grom="git rebase origin/main"
+alias grc="git rebase --continue"
+alias grs="git rebase --skip"
+alias gra="git rebase --abort"
 
 alias gd="git diff"
 alias gdt="git difftool"
 alias gs="git status"
 
-alias gco="git checkout"
+alias gc="git checkout"
 alias gcb="git checkout -b"
-alias gcm="git checkout master"
-
-alias grhead="git reset HEAD^"
-alias grhard="git fetch origin && git reset --hard"
+alias gcm="git checkout main"
 
 alias gst="git stash"
 alias gstp="git stash pop"
@@ -102,6 +94,8 @@ alias gstc="git stash clear"
 
 alias ggsup='git branch --set-upstream-to=origin/$(git_current_branch)'
 alias gpsup='git push --set-upstream origin $(git_current_branch)'
+
+alias gb="git branch"
 
 #-------------------------------------------------------------
 # tmux
